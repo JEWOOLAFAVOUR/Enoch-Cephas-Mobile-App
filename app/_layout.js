0406eb4779a0c72ab911f7f4e28b948c3ffc3192
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2024-09-02 12:08:47 +0100

    sjsjjs

--- app/_layout.js
+++ app/_layout.js
@@ -1,14 +1,23 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button, Image } from "react-native";
 
 const App = () => {
   return (
     <View style={styles.enoch}>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
+      <View style={{}}>
+        <Text style={{ fontSize: 30, fontWeight: 'bold', color: 'red' }}>RN Component</Text>
+
+        <View style={{ marginTop: 30, marginBottom: 30 }}>
+          <Button title="Press Me" />
+        </View>
+
+        <Image source={require("../assets/images/enoch.jpg")} style={{ height: 200, width: 200, }} />
+      </View>
+      <Image
+        source={{ uri: "https://img.freepik.com/free-photo/close-up-hand-wearing-chains_23-2151693716.jpg?t=st=1725274832~exp=1725278432~hmac=4a0c3f00f576ec0849a638559980ded9333dfca5d8dc42273137c5249c4b1dd9&w=360" }}
+        style={{ height: 200, width: 200, marginTop: 30, borderRadius: 200, }}
+      />
+
     </View>
   );
 };
@@ -19,7 +28,7 @@ const styles = StyleSheet.create({
   enoch: {
     flex: 1,
     backgroundColor: "white",
-    marginTop: 30,
-    paddingHorizontal: 10,
+    paddingTop: 40,
+    paddingHorizontal: 20,
   },
 });
